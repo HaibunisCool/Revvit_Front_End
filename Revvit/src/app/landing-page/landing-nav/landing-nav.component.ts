b72e22eb9d4e823b6fc9e5063c61a6f69b122acb
Phutondog <ericmphu@gmail.com>
2022-08-01 15:33:39 -0400

    landing page set-up

---
+++ Revvit/src/app/landing-page/landing-nav/landing-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-nav',
+  templateUrl: './landing-nav.component.html',
+  styleUrls: ['./landing-nav.component.scss']
+})
+export class LandingNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
