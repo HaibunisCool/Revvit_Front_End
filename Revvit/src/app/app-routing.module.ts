da95c9286f0991733be2d184edec9a1c5cadd71b
Hal Coronado <hallie_coronado1@baylor.edu>
2022-08-02 13:14:15 -0500

    started faq page

--- Revvit/src/app/app-routing.module.ts
+++ Revvit/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageModule } from './landing-page/landing-page.module';
+import { FaqPageComponent } from './landing-page/pages/faq-page/faq-page.component';
 import { LandingPageComponent } from './landing-page/pages/landing-page/landing-page.component';
 
 const routes: Routes = [{
   path: '',
   component: LandingPageComponent
+},
+{
+  path: 'faqs',
+  component: FaqPageComponent
 }];
 
 @NgModule({
--- Revvit/src/app/landing-page/landing-page.module.ts
+++ Revvit/src/app/landing-page/landing-page.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { LandingNavComponent } from './landing-nav/landing-nav.component';
-
+import { FaqPageComponent } from './pages/faq-page/faq-page.component';
 
 
 @NgModule({
   declarations: [
     LandingPageComponent,
-    LandingNavComponent
+    LandingNavComponent,
+    FaqPageComponent
   ],
   imports: [
     CommonModule
