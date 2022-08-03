927864fc50991d0adcab78dddc565b65e3a2e322
Phutondog <ericmphu@gmail.com>
2022-08-02 23:28:06 -0400

    organized code

--- Revvit/src/app/landing-page/pages/landing-page/landing-page.component.ts
+++ Revvit/src/app/landing-page/pages/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
+  }
+
+  goToLogin(){
+    this.route.navigateByUrl("/login");
+
+  }
+
+  goToSignup(){
+    this.route.navigateByUrl("/signup");
+
   }
 
 }
