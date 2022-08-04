6a8596857b466aec4053f29ab67d85bdbf498168
HaibunisCool <97471066+HaibunisCool@users.noreply.github.com>
2022-08-04 11:36:06 -0500

    added auth guard.

---
+++ Revvit/src/app/auth/auth.guard.ts
@@ -0,0 +1,25 @@
+import { AuthService } from './auth.service';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private _authService: AuthService, private router: Router){
+
+  }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if(this._authService.isLoggedIn()){
+      return true;
+    }else { 
+      alert("You are not logged in.")
+      this.router.navigate(['login']);
+      return false;
+    }
+  }
+  
+}
